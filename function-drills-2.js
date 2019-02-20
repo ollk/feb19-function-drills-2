85056fb9165abea930282e18da8ac17b2d9f75fd
Scott Williams <scottcolemanwilliams@gmail.com>
2019-02-19 19:27:42 -0600

    added decode

--- function-drills-2.js
+++ function-drills-2.js
@@ -25,3 +25,20 @@ function beyond(num) {
   }
   else {console.log('And beyond');}
 }
+
+
+function decode(word) {
+  if (word[0] === 'a') {
+    return word[1];
+  }
+  else if (word[0] === 'b') {
+    return word[2];
+  }
+  else if (word[0] === 'c') {
+    return word[3];
+  }
+  else if (word[0] === 'd') {
+    return word[4];
+  }
+  else {return ' ';}
+}
