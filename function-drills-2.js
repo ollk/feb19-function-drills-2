13096578fa7a7eaf0fcef865787a125563e2192a
Scott Williams <scottcolemanwilliams@gmail.com>
2019-02-19 23:17:10 -0600

    added rockPaperScissors

--- function-drills-2.js
+++ function-drills-2.js
@@ -75,3 +75,43 @@ function howManyDays(month, leapYear) {
     return `${month} ${days}`;
   }
 }
+
+
+//Trying to make it call out the moves, so i moved randomNo out for scope.
+//Now it stays the same until I refresh.
+const randomNo = Math.floor(Math.random() * 3) + 1;
+
+function rockPaperScissors(playerNo) {
+  if (playerNo < 1 || playerNo > 3 || typeof playerNo !== 'number' || playerNo % 1 !== 0) {
+    throw Error ('Please enter number between 1 and 3');
+  }
+  else if (playerNo === randomNo) {
+    return 'Tie';
+  } 
+  else if (playerNo + randomNo === 4) {
+    if (playerNo === 1) {
+      return 'Player Wins';
+    }
+    else {return 'Computer Wins';}
+  }
+  else if (playerNo > randomNo) {
+    return 'Player Wins';
+  }
+  else if (randomNo > playerNo) {
+    return 'Computer Wins';
+  }
+}
+
+function call(anyNo) {
+  if (anyNo > 2) {
+    return 'Scissors';
+  }
+  else if (anyNo < 2){
+    return 'Rock';
+  }
+  else {return 'Paper';}
+}
+
+function play(playerNo) { 
+  console.log(`Player plays ${call(playerNo)}, Computer plays ${call(randomNo)}, ${rockPaperScissors(playerNo)}`);
+}
